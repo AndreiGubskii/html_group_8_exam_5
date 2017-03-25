d76a50b822832c43f83175e9cba0f921f537d495
AndreiGubskii <andrei.gubskii@gmail.com>
2017-03-25 20:37:18 +0600

    добавил прокрутку по якорям, сверстал блок contact

--- js/main.js
+++ js/main.js
@@ -24,3 +24,18 @@ function showMenu(){
 function hideMenu(){
 	menu.animate({right: "-1000"},300, function() {menu.hide();});
 }
+
+// Плавная прокрутка по якорям
+menu.on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 700 мс
+    $('body,html').animate({scrollTop: top}, 700);
+});
